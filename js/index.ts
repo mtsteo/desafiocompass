4896ab521266e25b91dac7182193f13ca589bb89
mtsteo <mateusteotonho@gmail.com>
2024-02-27 00:18:31 -0300

    FEAT: Login logic Implemented

--- js/index.ts
+++ js/index.ts
@@ -1,10 +1,14 @@
 const modal = document.getElementById("modal")! as HTMLElement;
-
-const loginButton = document.getElementById("loginLink")! as HTMLElement;
-
+const loginLink = document.getElementById("loginLink")! as HTMLElement;
+const loginButton = document.getElementById(
+  "loginButton"
+)! as HTMLButtonElement;
 const closeButton = document.getElementsByClassName("close")[0] as HTMLElement;
 
-loginButton.onclick = () => {
+const EMAIL = "mateus@compass.com";
+const PASSWORD = "compass";
+
+loginLink.onclick = () => {
   modal.style.display = "block";
 };
 
@@ -12,8 +16,26 @@ closeButton.onclick = () => {
   modal.style.display = "none";
 };
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+window.onclick = function (event: MouseEvent) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+};
+
+loginButton.onclick = (e: MouseEvent) => {
+  e.preventDefault();
+  let email = (<HTMLInputElement>document.getElementById("emailInput")).value;
+  let password = (<HTMLInputElement>document.getElementById("passwordInput"))
+    .value;
+
+  loginValidate(email, password);
+};
+
+
+function loginValidate(email: string, password: string) {
+  if (email === EMAIL && password === PASSWORD) {
+    console.log("logged!");
+  } else {
+    alert("Fail! Check your data!")
+  }
+}
